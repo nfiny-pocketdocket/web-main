a57d2d8a0b5c121c7eeb02f34b0a7a0da27a3b16
Jae Nam <jae@nfiny.com>
2024-03-04 14:25:36 +1300

    wip

--- src/views/Homepage.tsx
+++ src/views/Homepage.tsx
@@ -1,5 +1,20 @@
 import { Flex } from "@chakra-ui/react";
 
 export const Homepage = () => {
-  return <Flex></Flex>;
+  return (
+    <Flex flexDir="column" overflowX="hidden">
+      <Part />
+      <Part bg="blue" />
+      <Part />
+      <Part bg="blue" />
+    </Flex>
+  );
+};
+
+export const Part = ({ bg = undefined }) => {
+  return (
+    <Flex w="100vw" h="100vh" bg={bg ?? "red"}>
+      Hello world
+    </Flex>
+  );
 };
